9557889d529072255acb9d4f9999745aa5e97ca6
Aryaman51 <aryamanjarial@gmail.com>
2026-03-14 13:31:23 +0530

    edited vite-config.json to include lb dns

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  server: { port: 3000 },
+  preview: {
+    allowedHosts: [
+      "expense-tracker-lb-1464887793.ap-south-1.elb.amazonaws.com"
+    ]
+  }
+
 });
